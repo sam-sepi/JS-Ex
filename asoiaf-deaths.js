3163306d2fe75ee73425123a85832d938d09c480
sam-sepi <sam-sepi.84@gmail.com>
2020-02-10 17:10:35 +0100

    fv

---
+++ asoiaf-deaths.js
@@ -0,0 +1,6 @@
+const asoiafDeaths = () => 
+{
+    return {"name":"Catelyn","house":"Stark","book":"A storm of swords"};
+};
+
+module.exports = asoiafDeaths;
--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
+const asoiafDeaths = require('./asoiaf-deaths');
 const PORT = 3000;
 
-const mw = (req, res, next) => {
-    return next;
-}
-
-app.use(mw);
+app.use(helmet());
 
 app.get('/', (req, res) => {
     res.status(201)
     .send('Listen');
 });
 
-app.get('/status', (req, res) => {
+app.get('/asoiaf-deaths', (req, res) => {
     res.status(200)
-    .send({Status: 200});
+    .send(asoiafDeaths());
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
+    console.log(`Server listening on port ${PORT}`);
 });
