d79319e2dbbe47d63f7ca25912234cce22686c22
sam-sepi <sam-sepi.84@gmail.com>
2020-02-10 16:26:57 +0100

    fv

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const mw = (req, res, next) => {
+    return next;
+}
+
+app.use(mw);
+
+app.get('/', (req, res) => {
+    res.status(201)
+    .send('Listen');
+});
+
+app.get('/status', (req, res) => {
+    res.status(200)
+    .send('Status');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+});
